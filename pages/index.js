d76a912b9bbf53d9c397bb8ade7078efb974e291
Eduard Alves <dumorango@gmail.com>
2020-07-13 18:58:14 -0500

    🚧 WIP

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import Head from "next/head";
-import useSWR from "swr";
-import Layout, { siteTitle } from "../components/layout";
-import utilStyles from "../styles/utils.module.css";
+import styles from "../styles/home.module.css";
 import { getSortedPostsData } from "../lib/posts";
 
 export async function getStaticProps() {
@@ -13,30 +11,43 @@ export async function getStaticProps() {
   };
 }
 
-const fetcher = (...args) => fetch(...args).then(res => res.json())
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+const TITLE = "Faz Bem - Receitas Low Carb";
+
+const HomeHead = () => (
+  <Head>
+    <link rel="icon" href="/favicon/favicon.ico" />
+    <title>{TITLE}</title>
+    <meta name="description" content="Receitas Low Carb e muito mais...." />
+    <meta
+      property="og:image"
+      content={`https://og-image.now.sh/${encodeURI(
+        "Faz Bem"
+      )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+    />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0"
+    ></meta>
+    <meta name="og:title" content={TITLE} />
+    <meta name="twitter:card" content="summary_large_image" />
+  </Head>
+);
 
 export default function Home({ allPostsData }) {
-  const { data, error } = useSWR("/api/hello", fetcher);
+  // const { data, error } = useSWR("/api/hello", fetcher);
   return (
-    <Layout home>
-      <Head>…</Head>
-      <section className={utilStyles.headingMd}>
-        {data?.name || "...loading"}
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
+    <div className={styles.container}>
+      <HomeHead />
+      <header className={styles.header}>
+        <div className={styles.logo}>
+          <img src="/images/logo.png" />
+          <div>Faz Bem</div>
+        </div>
+        <div>Faz Bem</div>
+      </header>
+      <main className={styles.content}>Main</main>
+    </div>
   );
 }
