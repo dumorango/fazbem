310c0e12f3ebaade37da6cc297cc2f911e49fb52
Eduard Alves <dumorango@gmail.com>
2020-08-18 23:22:46 -0500

    ✨ Create main layout and connect to Contentful

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,21 @@
-import { useWindowScroll } from "react-use";
 import Head from "next/head";
-import styles from "../styles/home.module.css";
-import { getSortedPostsData } from "../lib/posts";
+import * as contentful from "contentful";
+
+const contentfulClient = contentful.createClient({
+  space: "ys9m8lixnwxv",
+  accessToken: "8HGjN-MdMNvIFVtWbILHkHq8zZ7SU2VgxqxOQrd2L-0",
+});
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
+  const entries = await contentfulClient.getEntries();  
+  const recipes = entries.items.map(({ sys: { id }, fields }) => ({
+    id,
+    name: fields.nome,
+    imageUrl: fields.fotoPequena.fields.file.url,
+  }));  
   return {
     props: {
-      allPostsData,
+      recipes,
     },
   };
 }
@@ -16,46 +24,48 @@ const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 const TITLE = "Faz Bem - Receitas Low Carb";
 
-const HomeHead = () => (
-  <Head>
-    <link rel="icon" href="/favicon/favicon.ico" />
-    <title>{TITLE}</title>
-    <meta name="description" content="Receitas Low Carb e muito mais...." />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1.0"
-    ></meta>
-    <meta name="og:title" content={TITLE} />
-    <meta name="twitter:card" content="summary_large_image" />
-  </Head>
-);
-
-export default function Home({ allPostsData }) {
-  const imageRef = React.useRef(null);
-  const { x, y } = useWindowScroll();
-  console.log("x", x, "y", y);
-  const scrollRef = React.useRef(null);
-  const fazbem =
-    (scrollRef.current && scrollRef.current.getBoundingClientRect()) || {};
-  console.log("fazbem ", fazbem.y);
-  const shouldBeFixed = fazbem.y && fazbem.y <= 10;
-  const isImageOut =
-    imageRef.current && imageRef.current.getBoundingClientRect().y < -600;
+export default function Home({ recipes }) {  
   return (
     <>
-      <HomeHead />
-      <header className={styles.header}>
-        {/* <div className={styles.banner}>
-          <img ref={imageRef} src="/images/panqueca-lowcarb.jpg" />
-        </div> */}
-        <div className={styles.logo}>
-          <img src="/images/logo.png" />
+      <Head>
+        <link rel="icon" href="/favicon/favicon.ico" />
+        <title>{TITLE}</title>
+        <meta name="description" content="Receitas Low Carb e muito mais...." />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+        <meta name="og:title" content={TITLE} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <div className="flex justify-center">
+        <div className="max-w-screen-lg w-screen mx-8">
+          <header className="flex flex-row my-8 align-baseline">
+            <div className="flex h-16 w-16 z-10 mt-4">
+              <img src="/images/logo.png" />
+            </div>
+            <div className="font-display text-6xl text-center flex-1">
+              Faz Bem
+            </div>
+          </header>
+          <main>
+            <div className="flex flex-col">
+              <div className="flex flex-row justify-center flex-wrap my-16">
+                {recipes.map(({ name, imageUrl, id }) => (
+                  <div key={id} className="flex flex-col justify-center m-2 shadow cursor-pointer transform motion-reduce:transform-none hover:-translate-y-1 hover:scale-105 transition ease-in-out duration-300">
+                    <div className="flex w-56 h-64 flex-grow ">
+                      <img src={imageUrl} />
+                    </div>
+                    <div className="flex h-16 justify-center items-center">
+                      { name }
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </main>
         </div>
-        <div ref={scrollRef} className={styles.fazbem}>
-          Faz Bem
-        </div>
-      </header>
-      <main className={styles.content}>Main</main>
+      </div>
     </>
   );
 }
