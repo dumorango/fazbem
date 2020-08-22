a831131d3bc4b1befc074b5f156f7b05ee566176
eduardalves <eduard.alves@skipthedishes.ca>
2020-08-22 11:28:50 -0500

    Configure preview mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 module.exports = {
   purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    fontFamily: {        
-        'display': ["Gochi Hand"]
+    fontFamily: {
+      display: ["Gochi Hand"],
+      sans: ["Martel"],
     },
     extend: {
       colors: {
         "accent-1": "#333",
+        green: "#4e5f28",
       },
     },
   },
